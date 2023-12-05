92d362fc514d80d3ed44c4779d52ed4c384fa82b
SAMITHA <49331487+drsamitha@users.noreply.github.com>
2023-12-05 19:08:31 +0530

    files backup service
    need filesystem header

--- fileBackup.cpp
+++ fileBackup.cpp
@@ -1,64 +1,39 @@
-#include <windows.h>
-#include <string>
-#include <fstream>
 #include <iostream>
-#include <cstdio>
+#include <chrono>
+#include "filesystem.hpp"
+#include <string>
+#include <windows.h>
 
-void CopyFileToTemp(std::string filePath) {
-    FILE* tempFile = std::tmpfile();
-    if (tempFile == nullptr) {
-        std::cout << "Failed to create temporary file.\n\n";
-        return;
+namespace fs = ghc::filesystem;
+
+void copy_file_to_backup(const fs::path& file_path, const fs::path& backup_path) {
+    auto last_write_time = fs::last_write_time(file_path);
+    auto now = std::chrono::system_clock::now();
+    auto age = std::chrono::duration_cast<std::chrono::seconds>(now - last_write_time).count();
+    if (age > 60) {
+        fs::copy(file_path, backup_path, fs::copy_options::overwrite_existing);
     }
+}
 
-    std::ifstream src(filePath, std::ios::binary);
-    char buffer[4096];
-    while (src.read(buffer, sizeof(buffer)))
-        fwrite(buffer, 1, src.gcount(), tempFile);
-
-    std::cout << "Copied file: " << filePath << " to a temporary file.\n\n";
-
-    // Close the temporary file. The file is automatically deleted when the program exits or when the file is closed.
-    std::fclose(tempFile);
+void copy_files_to_backup_folder(const fs::path& folder_path) {
+    fs::path backup_folder_path = folder_path / "backup";
+    if (!fs::exists(backup_folder_path)) {
+        fs::create_directory(backup_folder_path);
+    }
+    else {
+        std::string narrow_path = backup_folder_path.string();
+        SetFileAttributesA(narrow_path.c_str(), FILE_ATTRIBUTE_HIDDEN);
+    }
+    for (const auto& entry : fs::directory_iterator(folder_path)) {
+        if (entry.is_regular_file()) {
+            fs::path backup_path = backup_folder_path / entry.path().filename();
+            copy_file_to_backup(entry.path(), backup_path);
+        }
+    }
 }
 
 int main() {
-    char buffer[MAX_PATH];
-    GetModuleFileName(NULL, buffer, MAX_PATH);
-    std::string::size_type pos = std::string(buffer).find_last_of("\\/");
-    std::string currentDir = std::string(buffer).substr(0, pos);
-
-    std::cout << "Monitoring directory: " << currentDir << "\n\n";
-
-    HANDLE hDir = CreateFile(currentDir.c_str(), FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-                              NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
-
-    if (hDir == INVALID_HANDLE_VALUE) {
-        std::cout << "Failed to open directory.\n\n";
-        return 1;
-    }
-
-    FILE_NOTIFY_INFORMATION Buffer[1024];
-    DWORD BytesReturned;
-
-    while (true) {
-        if ( ReadDirectoryChangesW(hDir, &Buffer, sizeof(Buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_ACCESS, &BytesReturned, NULL, NULL) ) {
-            FILE_NOTIFY_INFORMATION* fni = (FILE_NOTIFY_INFORMATION*)&Buffer;
-            if (fni->Action == FILE_ACTION_MODIFIED) {
-                std::wstring fileNameW(fni->FileName, fni->FileNameLength / sizeof(wchar_t));
-                std::string fileName(fileNameW.begin(), fileNameW.end());
-                std::string fullPath = currentDir + "\\" + fileName;
-
-                // Check if the modified file is a regular file and not a directory
-                DWORD attributes = GetFileAttributes(fullPath.c_str());
-                if (attributes != INVALID_FILE_ATTRIBUTES && !(attributes & FILE_ATTRIBUTE_DIRECTORY)) {
-                    std::cout << "File read: " << fileName << "\n\n";
-                    CopyFileToTemp(fullPath);
-                }
-            }
-        }
-    }
-
-    CloseHandle(hDir);
+    fs::path folder_path = fs::current_path();
+    copy_files_to_backup_folder(folder_path);
     return 0;
 }
